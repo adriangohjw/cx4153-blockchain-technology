ebeb866aeb2045ea3fc950c155de10c311990fc4
adriangohjw <adriangohjw@gmail.com>
2020-10-11 20:43:50 +0800

    Add getOwner() to owned contract

--- contracts/owned.sol
+++ contracts/owned.sol
@@ -2,11 +2,14 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract owned {
-    address owner;
+	address owner;
 
-    modifier onlyowner() {
-        require(msg.sender == owner);
-        _;
+	modifier onlyowner() {
+		require(msg.sender == owner);
+		_;
+	}
 
-    }
+	function getOwner() public view returns (address) {
+		return owner;
+	}
 }
