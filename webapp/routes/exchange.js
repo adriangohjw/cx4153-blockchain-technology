5e6a92fa4523c571556263d4961f0cb475db3309
adriangohjw <adriangohjw@gmail.com>
2020-11-02 04:23:08 +0800

    Setup ExchangeAPI config for local development

--- webapp/routes/exchange.js
+++ webapp/routes/exchange.js
@@ -9,19 +9,21 @@ var Web3 = require("web3");
 var artifact = require("../../build/contracts/Exchange.json");
 
 // infura endpoint
-const infuraWSS = `wss://ropsten.infura.io/ws/v3/5a68d74b5a074d7ab6834c50c65773b9`;
+const infuraWSS = `wss://ropsten.infura.io/ws/v3/5a68d74b5a074d7ab6834c50c65773b9`; // To change to your own infura endpoint
+const local_WSS = `ws://localhost:7545`;
 
 // contract address
-const ExchangeContractAddress = "0x1420180D527adfDcb7c17C8eA3a39C6b53b573Ab";
+const ExchangeContractAddress = "0x1420180D527adfDcb7c17C8eA3a39C6b53b573Ab"; // To change to your own testnet contract address
+const local_ExchangeContractAddress = "0x79307977Cc2969Eff40670E1341038D91E3854E2"; // To change to your own local contract address
 
 // testnet that contract is deployed on
 const Testnet = "ropsten";
 
 // setting up connection for web3
 let web3 = new Web3(
-  Web3.currentProvider || new Web3.providers.WebsocketProvider(infuraWSS)
+  Web3.currentProvider || new Web3.providers.WebsocketProvider(local_WSS)  // choose infuraWSS (ropsten testnet) / local_WSS (local)
 );
-const contract = new web3.eth.Contract(artifact.abi, ExchangeContractAddress);
+const contract = new web3.eth.Contract(artifact.abi, local_ExchangeContractAddress);  // choose ExchangeContractAddress (ropsten testnet) / local_ExchangeContractAddress (local)
 
 // Exchange Contract - getEtherBalanceInWei()
 async function getEtherBalanceInWei(addr) {
