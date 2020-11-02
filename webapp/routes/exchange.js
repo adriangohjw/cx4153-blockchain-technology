f174ed7dda473bfa2d8bfc9f0b48c18e44376d0b
adriangohjw <adriangohjw@gmail.com>
2020-11-02 15:51:17 +0800

    Add Exchange.hasToken API

--- webapp/routes/exchange.js
+++ webapp/routes/exchange.js
@@ -6,15 +6,20 @@ var router = express.Router();
 var Web3 = require("web3");
 
 // importing a compiled contract artifact which contains function signature etc. to interact
-var artifact = require("../../build/contracts/Exchange.json");
+var exchange_artifact = require("../../build/contracts/Exchange.json");
+var token_artifact = require("../../build/contracts/Token.json");
 
 // infura endpoint
 const infuraWSS = `wss://ropsten.infura.io/ws/v3/5a68d74b5a074d7ab6834c50c65773b9`; // To change to your own infura endpoint
 const local_WSS = `ws://localhost:7545`;
 
+// contract owner's address
+const OwnerAddress = "0xfC4E8AFeF7AD2a37DF8Ad0C3Ce038E4196501Fbe"; // To change to your own testnet wallet address
+const local_OwnerAddress = "0xc97f9e2a6469190894e403e7ea5e3620bfa790fd"; // To change to your own local wallet address
+
 // contract address
 const ExchangeContractAddress = "0x1420180D527adfDcb7c17C8eA3a39C6b53b573Ab"; // To change to your own testnet contract address
-const local_ExchangeContractAddress = "0x79307977Cc2969Eff40670E1341038D91E3854E2"; // To change to your own local contract address
+const local_ExchangeContractAddress = "0xE39726aD7Ad1079560EAE85085D408ade7407BBd"; // To change to your own local contract address
 
 // testnet that contract is deployed on
 const Testnet = "ropsten";
@@ -23,7 +28,19 @@ const Testnet = "ropsten";
 let web3 = new Web3(
   Web3.currentProvider || new Web3.providers.WebsocketProvider(local_WSS)  // choose infuraWSS (ropsten testnet) / local_WSS (local)
 );
-const contract = new web3.eth.Contract(artifact.abi, local_ExchangeContractAddress);  // choose ExchangeContractAddress (ropsten testnet) / local_ExchangeContractAddress (local)
+const contract = new web3.eth.Contract(exchange_artifact.abi, local_ExchangeContractAddress);  // choose ExchangeContractAddress (ropsten testnet) / local_ExchangeContractAddress (local)
+
+// Exchange Contract - hasToken
+async function hasToken(symbolName) {
+  try {
+    let isHasToken = await contract.methods.hasToken(symbolName).call();
+    return isHasToken;
+  }
+  catch (error) {
+    console.log(error);
+    return false;
+  }
+}
 
 // Exchange Contract - getEtherBalanceInWei()
 async function getEtherBalanceInWei(addr) {
@@ -41,6 +58,13 @@ async function getEtherBalanceInWei(addr) {
     };
   }
 }
+
+router.get('/hasToken', async function(req, res) {
+  response = await hasToken(req.query.tokenSymbol);
+  res.json({
+    hasToken: response
+  })
+});
 
 router.get('/getEtherBalanceInWei', async function(req, res) {
   if (req.query.addr) {
