bd5fd38d8cafb28e56e209f5392e03908f7c1ed2
adriangohjw <adriangohjw@gmail.com>
2020-11-03 06:19:47 +0800

    Add Exchange.sellToken API

--- webapp/routes/exchange.js
+++ webapp/routes/exchange.js
@@ -215,4 +215,19 @@ router.post('/buyToken', async function(req, res) {
   }
 })
 
+router.post('/sellToken', async function(req, res) {
+  try {
+    await contract.methods.sellToken(req.query.symbolName, req.query.priceInWei, req.query.amount)
+                          .send({ from: req.query.addr, gas: 1000000 });
+    res.json({
+      success: true
+    })
+  } catch (error) {
+    console.log(error);
+    res.json({ 
+      msg: `Error buying token`
+    })
+  }
+})
+
 module.exports = router;
