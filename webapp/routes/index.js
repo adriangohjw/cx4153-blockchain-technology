94fe5b61dc7f786e675895ccf3ac724e107342b1
adriangohjw <adriangohjw@gmail.com>
2020-11-02 20:26:41 +0800

    Add button to depositEther

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// blockchain http address
+const infuraHttp = `https://mainnet.infura.io/v3/5a68d74b5a074d7ab6834c50c65773b9`; // To change to your own infura endpoint
+const local_Http = `http://localhost:7545`;
+
+// contract address
+const ExchangeContractAddress = "0x1420180D527adfDcb7c17C8eA3a39C6b53b573Ab"; // To change to your own testnet contract address
+const local_ExchangeContractAddress = "0xE39726aD7Ad1079560EAE85085D408ade7407BBd"; // To change to your own local contract address
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index')
+  res.render('index', {
+    web3_http: local_Http,  // To change to infuraHttp for testnet
+    contract_address: local_ExchangeContractAddress // to change to ExchangeContractAddress for testnet
+  })
 });
 
 module.exports = router;
