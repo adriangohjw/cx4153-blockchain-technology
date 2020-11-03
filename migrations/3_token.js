6142fc2e3ccb0ee350489f5b07e682c12caa75be
adriangohjw <adriangohjw@gmail.com>
2020-11-03 20:44:05 +0800

    Update token decimals and supply

--- migrations/3_token.js
+++ migrations/3_token.js
@@ -3,7 +3,7 @@ const Token2 = artifacts.require("Token2");
 const Token3 = artifacts.require("Token3");
 
 module.exports = function (deployer) {
-  deployer.deploy(Token, "AdrianCoin", "ADC", 18, 1000000);
-  deployer.deploy(Token2, "BitConnect", "BCC", 18, 1000000);
-  deployer.deploy(Token3, "OneCoin", "ONE", 18, 1000000);
+  deployer.deploy(Token, "AdrianCoin", "ADC", 8, 1000000000);
+  deployer.deploy(Token2, "BitConnect", "BCC", 8, 1000000000);
+  deployer.deploy(Token3, "OneCoin", "ONE", 8, 1000000000);
 };
