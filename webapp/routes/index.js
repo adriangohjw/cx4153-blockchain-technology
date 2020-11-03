95493db247a04b998e6f14fa249852db13da4ba8
adriangohjw <adriangohjw@gmail.com>
2020-11-03 14:31:14 +0800

    Add display, interactiions for the exchange view

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -7,7 +7,7 @@ const local_Http = `http://localhost:7545`;
 
 // contract address
 const ExchangeContractAddress = "0x1420180D527adfDcb7c17C8eA3a39C6b53b573Ab"; // To change to your own testnet contract address
-const local_ExchangeContractAddress = "0xE39726aD7Ad1079560EAE85085D408ade7407BBd"; // To change to your own local contract address
+const local_ExchangeContractAddress = "0x98b7dE6C91529F1e0fC7039b4357105ec3379926"; // To change to your own local contract address
 
 /* GET home page. */
 router.get('/', function(req, res) {
