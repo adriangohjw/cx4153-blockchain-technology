9997aef87b5e393eb0db4dafeb023294155a4f7f
adriangohjw <adriangohjw@gmail.com>
2020-11-03 19:43:18 +0800

    Add more tokens for testing purposes

--- migrations/3_token.js
+++ migrations/3_token.js
@@ -1,5 +1,9 @@
 const Token = artifacts.require("Token");
+const Token2 = artifacts.require("Token2");
+const Token3 = artifacts.require("Token3");
 
 module.exports = function (deployer) {
-  deployer.deploy(Token, "AdrianCoin", "ADC", 8, 1000000);
+  deployer.deploy(Token, "AdrianCoin", "ADC", 18, 1000000);
+  deployer.deploy(Token2, "BitConnect", "BCC", 18, 1000000);
+  deployer.deploy(Token3, "OneCoin", "ONE", 18, 1000000);
 };
